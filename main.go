2a9d6d6945b2d5f067132560226b3f1d47a09bc4
Washington Lopes <washington.lopesdasilvafilho@gmail.com>
2023-08-17 14:24:44 -0400

    fix: find items with spaces " "

--- main.go
+++ main.go
@@ -1,37 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
 )
 
-// Declarar variáveis para armazenar as entradas do usuário
-var itemToSearch string
+// Declarar variáveis para armazenar as configurações e entradas do usuário
+var countryOptions = []string{
+	"AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG",
+	"PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO",
+}
 
 func main() {
+	var itemToSearch string
+	var country string
 
 	//Imprime o campo de item a procurar e armazena o input
 	fmt.Print("Item a procurar: ")
-	_, err := fmt.Scan(&itemToSearch)
+	reader := bufio.NewReader(os.Stdin)
+	itemToSearch, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Erro ao ler a entrada:", err)
 		os.Exit(1)
 	}
+	// Remove a quebra de linha do final
+	itemToSearch = strings.TrimSpace(itemToSearch)
 
-	// Defina as opções disponíveis
-	options := []string{"AM", "RJ", "SP", "BA"}
-
-	// Crie um prompt usando a biblioteca survey
-	var country string
+	// Cria um prompt usando a biblioteca survey
 	prompt := &survey.Select{
 		Message: "Estado a procurar: ",
-		Options: options,
+		Options: countryOptions,
 	}
 
-	// Pergunte ao usuário
+	// Pergunta ao usuário o estado em qual buscar o item
 	err = survey.AskOne(prompt, &country, survey.WithValidator(survey.Required))
 	if err != nil {
 		log.Fatal(err)
--- scraper.go
+++ scraper.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func scraper(item string, country string) {
+var totalItems int
+
+func scraper(itemToSearch string, country string) {
+
+	//formata os inputs do usuario
+	formatedItemToSearch := strings.ReplaceAll(itemToSearch, " ", "%20")
+	formatedCountry := strings.ToLower(country)
 
 	//recebe o item e o estado do item a procurar e logo em seguida cria um url
 	baseURL := "https://www.olx.com.br/estado-"
-	url := baseURL + country + "?q=" + item
+	url := baseURL + formatedCountry + "?q=" + formatedItemToSearch
 
 	// Faz a requisição HTTP para o URL e espera obter o conteúdo da página
 	response, err := http.Get(url)
@@ -28,22 +36,28 @@ func scraper(item string, country string) {
 		log.Fatal(err)
 	}
 
-	// Seletor CSS para o titulo da busca que o usuário deseja extrair
+	// Seletor CSS para o titulo da busca
 	searchTitle := "h1[data-ds-component='DS-Text']"
 
 	doc.Find(searchTitle).Each(func(index int, element *goquery.Selection) {
 		text := strings.TrimSpace(element.Text())
-		fmt.Println("Resultado da busca:", text)
+		fmt.Printf("Resultado da busca:%s\n", text)
+		fmt.Println(" ")
 	})
 
-	// Seletor CSS para os elementos que você deseja extrair
+	// Seletor CSS para os items referente a busca
 	searchItens := "h2[data-ds-component='DS-Text']"
 
 	doc.Find(searchItens).Each(func(index int, element *goquery.Selection) {
 		text := strings.TrimSpace(element.Text())
+		totalItems++
 		fmt.Printf("item %d: %s\n", index, text)
 	})
-
+	if totalItems <= 3 {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		fmt.Println("Itens não carregados corretamente, refazendo a busca...")
+		scraper(itemToSearch, country)
+	}
 }
-
-// <h2 data-ds-component="DS-Text" class="sc-jrQzAO jhFuLz horizontal title">Kindle 10° geração preto / Parcelo no cartão </h2>
